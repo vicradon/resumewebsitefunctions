8e2710af8554f970226a63f1ba149c0058505185
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2023-01-14 08:58:53 +0100

    chore: initial commit

---
+++ generate-preview/index.js
@@ -0,0 +1,20 @@
+const { createCanvas, loadImage } = require("canvas");
+
+module.exports = async function (context, req) {
+  const canvas = createCanvas(800, 600);
+  const ctx = canvas.getContext("2d");
+
+  ctx.font = "48px Impact";
+  ctx.fillText("Hello, OG World!", 50, 100);
+
+  const imageBuffer = canvas.toBuffer();
+
+  context.res = {
+    status: 202,
+    body: imageBuffer,
+    headers: {
+      "Content-Disposition": "inline",
+    },
+  };
+  context.done();
+};
